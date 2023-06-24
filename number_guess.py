9e6b6aeca0942dc83f3480dffe82f0a3b755b5c7
Aki78 <kmorooka78@gmail.com>
2023-06-24 15:47:28 +0300

    almost done number game, next, make it so that the user can type in the number.

--- number_guess.py
+++ number_guess.py
@@ -19,6 +19,7 @@ class NumberGuessingGame:
 
         self.entry = tk.Entry(self.main_frame)
         self.entry.pack()
+        self.entry.bind('<Return>', self.check_answer)  # bind Enter key to check_answer function
 
         self.check_button = tk.Button(self.main_frame, text="Check", command=self.check_answer)
         self.check_button.pack()
@@ -26,7 +27,7 @@ class NumberGuessingGame:
     def generate_random_number(self):
         return randint(*self.number_range)
 
-    def check_answer(self):
+    def check_answer(self, event=None):
         answer = self.entry.get()
         correct_answer = num2words(self.number_to_guess, lang='es')
 
@@ -37,9 +38,14 @@ class NumberGuessingGame:
             self.info_label.config(text="Correct! Now guess the next number.", fg='green')
         else:
             self.entry.delete(0, tk.END)
-            self.info_label.config(text="Incorrect! Try again.", fg='red')
+            self.info_label.config(text=f"Incorrect! The correct answer is '{correct_answer}'. Try again.", fg='red')
+            self.master.after(2000, self.reset_info_label)  # flash correct answer for 2 seconds
+
+    def reset_info_label(self):
+        self.info_label.config(text="Guess the number in Spanish", fg='black')
 
 if __name__ == "__main__":
     root = tk.Tk()
     game = NumberGuessingGame(root)
     root.mainloop()
+
